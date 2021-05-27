21b19a24c87219be9a8fa9fdb39a746fa73f74da
dnguyen <david.nguyen@smartsheet.com>
2021-05-26 23:42:32 -0700

    add test project to main project

---
+++ GraphVizTestProject/Main.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cpsc5031_hw6;
+
+namespace GraphVizTestProject
+{
+    [TestClass]
+    public class Main
+    {
+        [TestMethod]
+        public void TestCase_1_HappyPath()
+        {
+            Program graph = new Program();
+
+        }
+    }
+}
--- cpsc5031_hw6/Program.cs
+++ cpsc5031_hw6/Program.cs
@@ -5,7 +5,7 @@ using System.IO;
 
 namespace cpsc5031_hw6
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
